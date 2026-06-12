ce28eac7b77a1de04610cfa38b459120ad63daac
Khuslal Gupta <guptakhuslal@gmail.com>
2026-06-12 12:53:01 +0545

    login controller and register controller implemented successfully

--- springdemo/src/main/java/com/example/demo/controller/LoginController.java
+++ springdemo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
 
 @Controller
 public class LoginController {
@@ -9,4 +14,15 @@ public class LoginController {
 	public String getLogin() {
 		return "login";
 	}
+	
+	@PostMapping("/LoginController")
+	public String postLogin(@ModelAttribute User u, Model model) {
+		if(u.getUsername().equals("admin") && u.getPassword().equals("admin")) {
+			model.addAttribute("username", u.getUsername());
+			return "home";
+		}
+		model.addAttribute("error", "Invalid username or password");
+		return "login";
+	}
+
 }
--- springdemo/src/main/java/com/example/demo/controller/RegisterController.java
+++ springdemo/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
 
 @Controller
 public class RegisterController {
@@ -10,9 +14,14 @@ public class RegisterController {
 	public String getRegister() {
 		return "register";
 	}
-	
+
 	@PostMapping("/RegisterController")
-	public String postRegister() {
+	public String postRegister(@ModelAttribute User u, Model model) {
+
+		model.addAttribute("fname", u.getFname());
+		model.addAttribute("lname", u.getLname());
+		model.addAttribute("username", u.getUsername());
+
 		return "profile";
 	}
 }
